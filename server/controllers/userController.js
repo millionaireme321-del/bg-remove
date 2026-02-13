ce889a87523cf10b875389f7938fbbb36f11851a
Abhishek Adhikari <techpersonmax@gmail.com>
2026-02-14 02:46:12 +0545

    hii

---
+++ server/controllers/userController.js
@@ -0,0 +1,8 @@
+
+
+// API Controller Function to manage clerk with database
+// http://localhost:4000/api/user/webhooks
+
+const clerkWebhooks = async(req,res) => {
+    
+}
---
+++ server/server.js
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+
+// App Config
+const PORT = process.env.port || 4000
+const app = express()
+await connectDB()
+
+// Initialize Middleware
+app.use(express.json())
+app.use(cors())
+
+// API ROUTES
+app.get('/',(req,res)=> res.send("API WORKING"))
+
+app.listen(PORT, ()=> console.log("Server running on port " + PORT))
